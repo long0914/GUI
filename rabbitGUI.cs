22e140c279e0b2e0ea0082d0f438715b6bb9be56
long0914 <119887736+long0914@users.noreply.github.com>
2024-03-09 21:46:49 +0000

    mud

---
+++ rabbitGUI.cs
@@ -0,0 +1,27 @@
+using System.Dynamic;
+
+public interface ISingable
+{
+    abstract string Sing { get; set; }
+}
+
+public interface IFuckable{
+    void Fuck();
+
+}
+
+
+class Bird : ISingable, IFuckable
+{
+    private string _sing;
+    public string Sing{
+        get { return _sing; }
+        set { _sing = value; }
+        
+    }
+    public void Fuck(){
+        Console.WriteLine("Fucking");
+    }
+
+}
+
